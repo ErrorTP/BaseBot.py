cd125cd078c9711d2595a9939abd5f875bcbbd25
ErrorTheProgrammer <57261793+ErrorTheProgrammer@users.noreply.github.com>
2020-09-07 23:54:47 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import discord
+import asyncio
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!ping'):
+       await client.send_message(message.channel, 'Pong!')
+
+client.run('NzUyNzM4MDA2ODI1MzA0MTE2.X1b_5g.ZMAgAK937r5phr-OnVRDkI1n1_o')
