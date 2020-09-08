7d55938461ae30d2957d392c9f42eb14f9ae660e
ErrorTheProgrammer <57261793+ErrorTheProgrammer@users.noreply.github.com>
2020-09-07 23:59:53 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,4 +15,4 @@ async def on_message(message):
     if message.content.startswith('!ping'):
        await client.send_message(message.channel, 'Pong!')
 
-client.run('NzUyNzM4MDA2ODI1MzA0MTE2.X1b_5g.ZMAgAK937r5phr-OnVRDkI1n1_o')
+client.run('NzUyNzM4MDA2ODI1MzA0MTE2.X1b_5g.p-kWeZ5QAQgKAgYXHZ7XnF9om4M')
